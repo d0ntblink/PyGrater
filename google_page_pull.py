7b9443437e3d27087d542c0acdd540eeb0dc6713
gary@dontblink.ca <33300872+d0ntblink@users.noreply.github.com>
2021-02-18 18:09:56 -0800

    html file out pull is ready

---
+++ google_page_pull.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from random import randrange
+import requests
+from requests.api import get
+
+A = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+       )
+
+def create_url():
+    search_string = input("What is your question ? ")
+    url = search_string.replace(' ', '+')
+    url = 'https://google.com/search?q=' + url
+    return url
+
+
+def get_html(url):
+    user_agent = A[randrange(len(A))]
+    headers = {'user-agent': user_agent}
+    html_content = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(html_content, "lxml")
+    return soup
+
+
+print(get_html(url=create_url()))
