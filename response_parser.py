86e5c4e429bd3206151b730f4dcdb8faba02f850
Zachary Claypack <56490116+zclaypack@users.noreply.github.com>
2021-02-22 15:52:01 -0800

    Add more question types

--- response_parser.py
+++ response_parser.py
@@ -3,10 +3,14 @@ from bs4 import BeautifulSoup
 
 def parser(soup):
     # Find the answer
-    answer = soup.find("div", class_="BNeawe iBp4i AP7Wnd")
+    answerBasic = soup.find("div", class_="BNeawe iBp4i AP7Wnd")
+    answerAdv = soup.find("div", class_="BNeawe s3v9rd AP7Wnd")
+
 
     # Clean The Answer and Create Conditon for No Answer
-    if answer == None:
+    if answerBasic and answerAdv == None:
         return "Suprise! Google does not know the answer to that question!"
-    else:
-        return answer.text
+    elif (answerBasic == None) and (answerAdv != None):
+    	return answerAdv.text
+    elif (answerAdv == None) and (answerBasic != None):
+    	return answerBasic.text
