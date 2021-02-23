b7e6169353554dc17802a6e42f4ece52d8f0d650
Michael <47308440+EmbersAtDawn@users.noreply.github.com>
2021-02-22 16:04:34 -0800

    Whoops, forgot the message. Added inline arguments for python file running.

--- main.py
+++ main.py
@@ -9,12 +9,36 @@
 # 
 #  -------------------------------------------------------
 
+import sys, getopt
 from string_beautifier import inputCleanser
 from google_page_pull import create_url, get_html
 from response_parser import parser
 
+#  Input: System read arguments from command line
+# Output: User's input question
+#   Misc: -h and --help return usage instructions
+def checkArgs(argv):
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hi:', ['help', 'question='])
+    except getopt.GetoptError:
+        print('main.py -i "Your question here."')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ['-h', '--help']:
+            print('usage: main.py -i "Your question here."')
+            print('       main.py --question "Your question here."')
+            sys.exit()
+        elif opt in ['-i', '--question']:
+            inlineQuestion = inputCleanser(arg)
+            return inlineQuestion
+
 if __name__ == "__main__":
-    userQuestion = inputCleanser(input('What would you like to ask Google? '))
+    inlineQuestion = checkArgs(sys.argv[1:])
+
+    if inlineQuestion:
+        userQuestion = inlineQuestion
+    else:
+        userQuestion = inputCleanser(input('What would you like to ask Google? '))
 
     htmlResponse = get_html(url=create_url(question=userQuestion))
     # print(htmlResponse)  # this is for testing
