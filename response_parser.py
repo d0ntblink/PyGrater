feef416c602b6c73b7a87d125fa609f7b6734b61
Zachary Claypack <56490116+zclaypack@users.noreply.github.com>
2021-02-22 17:09:08 -0800

    Cleaned Answers

--- response_parser.py
+++ response_parser.py
@@ -6,11 +6,14 @@ def parser(soup):
     answerBasic = soup.find("div", class_="BNeawe iBp4i AP7Wnd")
     answerAdv = soup.find("div", class_="BNeawe s3v9rd AP7Wnd")
 
-
     # Clean The Answer and Create Conditon for No Answer
-    if answerBasic and answerAdv == None:
-        return "Suprise! Google does not know the answer to that question!"
+    if (answerBasic and answerAdv) != None:
+        return(answerBasic.text)
     elif (answerBasic == None) and (answerAdv != None):
-    	return answerAdv.text
-    elif (answerAdv == None) and (answerBasic != None):
-    	return answerBasic.text
+        return(answerAdv.text)
+    else:
+        return("Google Does Not Know!")
+
+    
+  
+  
